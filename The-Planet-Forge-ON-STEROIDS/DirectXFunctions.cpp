5b6005a711f2af24eabdd27e7e32ead041704640
volpers <nlskleine@gmail.com>
2017-08-28 00:28:27 +0200

    Initial Commit!

---
+++ The-Planet-Forge-ON-STEROIDS/DirectXFunctions.cpp
@@ -0,0 +1,33 @@
+﻿#include "stdafx.h"
+#include "DirectXFunctions.h"
+
+using namespace DirectX;
+
+
+DirectXFunctions::DirectXFunctions(HINSTANCE__* hinstance)
+{
+	initializeDirectX(hinstance);
+}
+
+void DirectXFunctions::releaseObjects() const
+{
+}
+
+bool DirectXFunctions::initScene()
+{
+	return true;
+}
+
+void DirectXFunctions::updateScene()
+{
+}
+
+void DirectXFunctions::drawScene()
+{
+}
+
+void DirectXFunctions::initializeDirectX(HINSTANCE__* hinstance)
+{
+}
+
+
---
+++ The-Planet-Forge-ON-STEROIDS/DirectXFunctions.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#pragma comment(lib, "d3d11.lib")
+
+#include <d3d11_1.h>
+#include <boost/shared_ptr.hpp>
+#include <directxcolors.h>
+#include "resource.h"
+
+
+class DirectXFunctions
+{
+public:
+	explicit DirectXFunctions(HINSTANCE__* hinstance);
+
+	void releaseObjects() const;
+	bool initScene();
+	void updateScene();
+	void drawScene();
+private:
+	void initializeDirectX(HINSTANCE__* hinstance);
+
+};
